e8afd299090c2c6ebec9e0ebbf5a54111626271e
faisal <pathanfaisalkhan313@gmail.com>
2026-05-24 10:06:22 +0530

    changed the url

--- src/main/java/com/hirex/config/SecurityConfig.java
+++ src/main/java/com/hirex/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
 //            "http://localhost:3000"
-                "https://job-portal-frontend-avtp.onrender.com"
+                "https://frontend-hirex.onrender.com"
 
         ));
 
